042d7b2c152dd093178bf3531621e8bbd9c80b56
Drakky3030 <topgund1998@gmail.com>
2016-03-05 20:55:36 -0500

    fagigloo

--- js/config.js
+++ js/config.js
@@ -77,15 +77,14 @@ var l_bgDarkening = 50;
 
 /*Music player
 'true' or 'false' to enable music in the background*/
-var l_music = true;
+var l_music = false;
 
 /*Display actual song's name?*/
-var l_musicDisplay = true;
+var l_musicDisplay = false;
 
 /*Music playlist
 Place how much Youtube IDs/.ogg you want*/
 var l_musicPlaylist = [
-	{youtube: "RPto4nRUsB8", name: "Otto Croy"}
 ];
 
 /*Random music order?*/
